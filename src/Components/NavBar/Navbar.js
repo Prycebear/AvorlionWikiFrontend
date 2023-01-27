9d8f7b14a369fadfd9bcf04534b6b9a49f1085fc
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-01-27 12:52:58 +0000

    Added React routing.

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -3,7 +3,9 @@ import "./Navbar.css"
 export default function Navbar() {
     return (
         <div className = "nav">
+            <div id = "Title-Background">
             <h1 id = "Title">Avorlion</h1>
+            </div>
             <ul className="NavElement">
                 <li><a  href="src/Components/NavBar/Navbar">Maps</a></li>
                 <li><a href="src/Components/NavBar/Navbar" >Characters</a></li>
