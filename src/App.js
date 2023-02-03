20bbd596e03ea1da15cbca8246d3ccaa2d4a9ae5
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-02-03 13:39:38 +0000

    Cards now fit and remain the same size.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Navbar from "./Components/NavBar/NavBar";
-import {CardBackground} from "./Pages/Background/CardBackground";
+import {CardBackground} from "./Pages/Background/CharacterCard/CardBackground";
 import {Maps} from "./Pages/Maps/Maps";
 import {MainPost} from "./Pages/PostPage/MainPostPage/MainPost";
 import GodCard from "./Components/Cards/GodCards/GodCard";
+import {GodCardBackground} from "./Pages/Background/GodCards/GodCardBackground";
 
 
 
@@ -16,7 +17,7 @@ function App() {
                     <Route path="/" element={<Navbar />}>
                         <Route index element={<Home />} />
                         <Route path="character" element={<CardBackground />} />
-                        <Route path="gods" element={<GodCard />} />
+                        <Route path="gods" element={<GodCardBackground />} />
                         <Route path="maps" element={<Maps />} />
                         <Route path="add" element={<MainPost />} />
 
