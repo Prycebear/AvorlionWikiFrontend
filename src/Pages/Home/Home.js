6e851d041ba9b947293886ca116dce8f199b0534
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-04-26 09:23:24 +0100

    Added map to home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,10 +5,11 @@ export default function Home() {
     return (
         <div className="Home">
             <div className="Title">
-                <h1 className="Title">Welcome to the Avorion Wiki!</h1>
+                <h1 className="Title">Welcome to the Avorlion Wiki!</h1>
             </div>
             <div className="Body">
                 <p>Ill add more to this as time progresses!</p>
+                <img src="src/Images/Maps/Keolon.png"/>
             </div>
 
         </div>
