ecf667a5cdc72cf2bc6ad13ed818897412ac51a3
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-04-27 08:52:54 +0100

    Added require image to main page.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,7 +8,8 @@ export default function Home() {
                 <h1 className="Title">Welcome to the Avorlion Wiki!</h1>
             </div>
             <div className="Body">
-                <p>Ill add more to this as time progresses!</p>
+                <p>Welcome to the world of Avorlion, a land of magic, monsters, and adventure. It is currently the year 1106, over a millennium since the Great Cataclysm that reshaped the world. The Cataclysm was a devastating event that shook the very foundations of reality, unleashing chaotic magic and bringing about massive destruction.
+                    In the aftermath, civilizations crumbled, new ones rose from the ashes, and the world was forever changed. Now, the people of Avorlion struggle to survive in a world where danger lurks around every corner and the forces of good and evil battle for supremacy.</p>
                 <img src={require('src/Images/Maps/Keolon.png')} alt="This is meant to load but that a problem for another day"/>
             </div>
 
