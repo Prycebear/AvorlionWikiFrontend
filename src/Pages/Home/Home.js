54d18a161d7ee327e723dfefd45b43368d27d53e
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-04-28 11:03:04 +0100

    Image now works and background scales to fit the page.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.css"
 
-import logo from "Keolon.png"
+import logo from "./Keolon.png"
 
 export default function Home() {
 
@@ -12,7 +12,10 @@ export default function Home() {
             <div className="Body">
                 <p>Welcome to the world of Avorlion, a land of magic, monsters, and adventure. It is currently the year 1106, over a millennium since the Great Cataclysm that reshaped the world. The Cataclysm was a devastating event that shook the very foundations of reality, unleashing chaotic magic and bringing about massive destruction.
                     In the aftermath, civilizations crumbled, new ones rose from the ashes, and the world was forever changed. Now, the people of Avorlion struggle to survive in a world where danger lurks around every corner and the forces of good and evil battle for supremacy.</p>
-                <img src={logo} alt="This is meant to load but that a problem for another day"/>
+                <img id="MainMap" src={logo} alt="This is meant to load but that a problem for another day"/>
+                <p>Welcome to the world of Avorlion, a land of magic, monsters, and adventure. It is currently the year 1106, over a millennium since the Great Cataclysm that reshaped the world. The Cataclysm was a devastating event that shook the very foundations of reality, unleashing chaotic magic and bringing about massive destruction.
+                    In the aftermath, civilizations crumbled, new ones rose from the ashes, and the world was forever changed. Now, the people of Avorlion struggle to survive in a world where danger lurks around every corner and the forces of good and evil battle for supremacy.</p>
+
             </div>
 
         </div>
