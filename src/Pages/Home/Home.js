f1322d5e0dcadb80fef65e22a38781eeebb914e6
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-04-28 10:39:11 +0100

    Maybe fixed an issue with the image on the homepage loading

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,7 @@
 import "./Home.css"
 
+import logo from "src/Pages/Home/Keolon.png"
+
 export default function Home() {
 
     return (
@@ -10,7 +12,7 @@ export default function Home() {
             <div className="Body">
                 <p>Welcome to the world of Avorlion, a land of magic, monsters, and adventure. It is currently the year 1106, over a millennium since the Great Cataclysm that reshaped the world. The Cataclysm was a devastating event that shook the very foundations of reality, unleashing chaotic magic and bringing about massive destruction.
                     In the aftermath, civilizations crumbled, new ones rose from the ashes, and the world was forever changed. Now, the people of Avorlion struggle to survive in a world where danger lurks around every corner and the forces of good and evil battle for supremacy.</p>
-                <img src={require('src/Images/Maps/Keolon.png')} alt="This is meant to load but that a problem for another day"/>
+                <img src={logo} alt="This is meant to load but that a problem for another day"/>
             </div>
 
         </div>
