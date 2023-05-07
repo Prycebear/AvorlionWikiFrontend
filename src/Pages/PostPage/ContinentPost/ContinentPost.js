ee87c0f163a80c87d2e2283b9bfdb9dd4a2d9ae2
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-05-07 21:33:23 +0100

    Added Continent post.

---
+++ src/Pages/PostPage/ContinentPost/ContinentPost.js
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react";
+import "./CharacterPost.css"
+
+
+export const ContinentPost = () => {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            let res = await fetch("https://avorlionbackend.herokuapp.com/continent/add", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: name,
+                    description: description,
+                    image: image,
+                }),
+            });
+            //let resJson = await res;
+            if (res.status === 200) {
+                setName("");
+                setDescription("");
+                setImage("");
+            } else {
+                console.log(res);
+            }
+        } catch (err) {
+            console.log(err);
+            return alert("Nah, no beuno on the posterino")
+        }
+        return alert("New character succesfully added broski!")
+    };
+
+
+    return (
+        <div className="CharacterPost">
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input type="text" id="name" name="name" value={name}
+                       onChange={(e) => setName(e.target.value)}/>
+                <br/>
+                <label>Description:</label>
+                <br/>
+                <textarea type="text" id="description" name="description" value={description}
+                          onChange={(e) => setDescription(e.target.value)}/>
+                <br/>
+                <label>Image link:</label>
+                <input type="text" id="image" name="image" value={image} onChange={(e) => setImage(e.target.value)}/>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>)
+
+
+}
+
+
+
