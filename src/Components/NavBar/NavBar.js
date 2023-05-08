3aa3ded48d4f46989ea4170aac18e3cc99634411
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-05-08 15:20:49 +0100

    Subnation to nation.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,6 +2,9 @@ import "./NavBar.css"
 import {Link, Outlet} from "react-router-dom";
 
 export default function Navbar() {
+
+
+
     return (
 
     <>
@@ -12,9 +15,10 @@ export default function Navbar() {
                 </div>
                 <ul className="NavElement">
                     <li><Link to="/">Home</Link></li>
+                    <li><button onClick={handleOpen}>Wiki</button></li>
                     <li><Link to="/character">Character</Link></li>
                     <li><Link to="/gods">Gods</Link></li>
-                    <li><Link to="/maps">Maps</Link></li>
+                    <li><Link to="/maps">Locations</Link></li>
                     <li><Link to="/add">Add</Link></li>
                 </ul>
             </div>
