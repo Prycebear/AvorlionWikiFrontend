ab3e9f426d6ecf64583f51f238cf823a3a42d32a
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-06-14 19:05:18 +0100

    Fixed a ton of issues, cards working better.

--- src/Components/Cards/DetailCards/DetailCharacter/DetailCharacter.js
+++ src/Components/Cards/DetailCards/DetailCharacter/DetailCharacter.js
@@ -10,17 +10,20 @@ export const DetailCharacter = () => {
 
     return (
         <div className="DetailCharacter" key={data.id}>
-            <h1>{data.firstName}</h1>
+            <h1>{data.firstName} {data.surname}</h1>
+
+            <br/>
             <div className="DataMain">
 
                 <div className="CharacterDescription">
+                    <h2>Description</h2>
                     <p className="DetailDesc">{data.description}</p>
                     <br/>
+                    <h2>History</h2>
                     <p className="DetailHist">{data.history}</p>
                 </div>
-                <div className="SideBarInfoCharacterDetails">
-                    <img className="DetailPicture" src = {data.imgLink}/>
-                    <ul>
+                <div id="SideBarInfoCharacterDetails">
+                    <img id="DetailPicture" src = {data.imgLink}/>
                         <h3>Born Location</h3>
                         <li>Born Location</li>
                         <h3>Born Date</h3>
@@ -37,39 +40,10 @@ export const DetailCharacter = () => {
                         <li>Affiliations</li>
                         <h3>Alignment</h3>
                         <li>Alignment</li>
-
-                    </ul>
                 </div>
             </div>
 
         </div>
-        // <div className="CharacterWrap">
-        //             <div className="CharacterCard">
-        //                 <div className="CharacterImage">
-        //                     <img
-        //                         src={data.imgLink}
-        //                         onError={({ currentTarget }) => {
-        //                             currentTarget.onerror = null; // prevents looping
-        //                             currentTarget.src="https://southernpercussion.com/wp-content/uploads/2021/09/placeholder.png";
-        //                         }}
-        //                         alt="MainPic"
-        //                     />
-        //                 </div>
-        //                 <div id="card-interior">
-        //
-        //                     <div className="CardContent">
-        //                         <h3 className="CharacterName">{data.firstName}</h3>
-        //                         <ul className="CharacterDetails">
-        //                             <li id="Race"><h3>{data.race}</h3></li>
-        //                             <li id="Role"><h3>{data.role}</h3></li>
-        //                         </ul>
-        //                         <p id="desc">{data.description}</p>
-        //                     </div>
-        //                 </div>
-        //
-        //             </div>
-        // </div>
-
     )
 }
 
